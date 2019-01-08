bfd299f4dc80455416f31ccb9548266e09389085
Simon-Leonard <43200739+Simon-Leonard@users.noreply.github.com>
2019-01-08 15:36:12 +0100

    Create script.R

---
+++ example/script.R
@@ -0,0 +1,15 @@
+# Load Package :
+library(APERO)
+
+# Load annotation file
+ptt=read.table("E:/APERO/NC_016810.ptt",sep="\t",skip=2,header=T,stringsAsFactors = F)
+
+# 5'end detection
+res=APERO_start_detection(work_dir = "E:/APERO/", bam_name = "SRR2149882_position_1_to_10kb.bam", ptt_file =ptt
+                          , wmax = 10, min_dist = 10, enrichment = 0.1, min_read_number = 0, genome_size = 4878012)
+
+
+#3'end detection
+res2=APERO_end_detection(work_dir = "E:/APERO/", start_table = res, mTEX_bam = "SRR2149882_position_1_to_10kb.bam"
+                         , readthrough_proportion = 0.01, Fmin=NA, thread_number = 8, genome_size = 4878012
+                         , ptt_file=ptt)
